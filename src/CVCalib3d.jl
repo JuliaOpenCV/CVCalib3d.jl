d3103b9a951ab79d312767dfd4d79fa233543f3f
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-07-13 00:03:42 +0900

    init

--- src/CVCalib3d.jl
+++ src/CVCalib3d.jl
@@ -1,5 +1,19 @@
 module CVCalib3d
 
-# package code goes here
+using LibOpenCV
+using CVCore
+using Cxx
+
+libopencv_calib3d = LibOpenCV.find_library_e("libopencv_calib3d")
+try
+    Libdl.dlopen(libopencv_calib3d, Libdl.RTLD_GLOBAL)
+catch e
+    warn("You might need to set DYLD_LIBRARY_PATH to load dependencies proeprty.")
+    rethrow(e)
+end
+
+cxx"""
+#include <opencv2/calib3d.hpp>
+"""
 
 end # module
